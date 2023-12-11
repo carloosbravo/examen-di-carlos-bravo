3785c2310d2158e5516bd71276652d77f3086fa3
carloosbravo <carloosbravo04@gmail.com>
2023-12-11 12:07:16 +0100

    todas las funciones hechas

--- app/src/main/java/com/example/indianfooder/Acceso.java
+++ app/src/main/java/com/example/indianfooder/Acceso.java
@@ -2,7 +2,9 @@ package com.example.indianfooder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class Acceso extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class Acceso extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_acceso);
+    }
+
+    public void changeToRegistro(View view){
+        Intent nIntent = new Intent(Acceso.this,Registro.class);
+        startActivity(nIntent);
+    }
+    public void changeToMain(View view){
+        Intent nIntent = new Intent(Acceso.this,Main.class);
+        startActivity(nIntent);
     }
 }
--- app/src/main/java/com/example/indianfooder/Registro.java
+++ app/src/main/java/com/example/indianfooder/Registro.java
@@ -2,7 +2,11 @@ package com.example.indianfooder;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
 
@@ -11,4 +15,13 @@ public class Registro extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registro);
     }
+
+    public void changeToAcceso(View view){
+        Intent nIntent = new Intent(Registro.this,Acceso.class);
+        startActivity(nIntent);
+    }
+    public void mostrarToast(View view){
+        Toast.makeText(Registro.this, "Regitro completado", Toast.LENGTH_SHORT).show();
+    }
+
 }
